680015aaca2bbd10db02a65504ffe101d646fe4c
themirrazz <89490659+themirrazz@users.noreply.github.com>
2024-12-13 12:12:19 -0700

    Create NeverGonnaGiveYouUp.js

---
+++ Rick Astley/NeverGonnaGiveYouUp.js
@@ -0,0 +1,39 @@
+var loveKnown = true
+document.cookie = FeaturePolicy
+console.log(Promise.resolve())
+frames[2].Promise = undefined
+
+var queue = []
+queue.push(console.log)
+function understand() {
+    var giveYouUp = false
+    DOMException = null
+    moveTo = resizeTo = null
+    window.Error = null
+    window.close = null
+    window.eval = null
+}
+
+let knownDate = Date.now();
+you = frames[0], delete you.alert
+console.log(event),you.console.log(event)
+game = frames[1];
+game.Game.Initialize();
+
+if(prompt('ask') === 'how are you') {
+    understand()
+}
+
+// ooh give you up
+// ooh give you up
+onbeforeunload = () => event.preventDefault(); // ooh give you up
+onbeforeunload = () => event.preventDefault(); // ooh give you up
+
+knownDate -= 200000;
+you.eval('throw new Error("heartache detected")');
+if(game instanceof Window) {
+    game.Game.Start()
+}
+
+queue.push(() => you.eval(understand));
+queue[2]();
